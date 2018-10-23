02fbc2b11a3b646730dd9160c9d2c5af7329bd7a
Angelos Paraskevaidis <angelos.paraskevaidis@factset.com>
2018-10-23 22:47:38 +0300

    first commit

---
+++ database/migrations/2018_10_23_100738_create_software_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSoftwareTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('software', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('software_id');
+            $table->string('licence');
+            $table->string('expired');
+            $table->text('version');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('software');
+    }
+}
---
+++ vagrant/after.sh
@@ -0,0 +1,4 @@
+echo "API - Setting up CACHE dir..."
+sudo mkdir -p /home/webchef/cache
+sudo chmod 0777 /home/webchef/cache
+
